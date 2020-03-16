dcd8743ce5abdfd0b79b905dc70efed819c2040f
ramzysaad <57734793+ramzysaad@users.noreply.github.com>
2020-03-15 21:48:48 -0400

    RoO and Conference pages
    RoO and Conference pages

---
+++ AIRoO/SPA Login/TodoSPA/App/Scripts/homeCtrl.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+angular.module('todoApp')
+    .controller('homeCtrl', ['$scope', 'adalAuthenticationService', '$location', function ($scope, adalService, $location) {
+    //Assign to login button in index.html
+    $scope.login = function () {
+        adalService.login();
+    };
+    $scope.logout = function () {
+        adalService.logOut();
+    };
+    $scope.isActive = function (viewLocation) {        
+        return viewLocation === $location.path();
+    };
+}]);
