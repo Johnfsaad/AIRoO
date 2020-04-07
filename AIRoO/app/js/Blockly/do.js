bb9f093a01c29caac8b00491d010e9a09b10fb01
ramzysaad <57734793+ramzysaad@users.noreply.github.com>
2020-04-07 15:00:39 -0400

    do.js
    do.js

--- AIRoO/app/js/Blockly/do.js
+++ AIRoO/app/js/Blockly/do.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['do'] = {
   init: function() {
     this.appendDummyInput("do")
-        .appendField("DoS")
+        .appendField("Do")
         .appendField(new Blockly.FieldTextInput("default"), "NAME");
     this.setColour(230);
  this.setTooltip("");
