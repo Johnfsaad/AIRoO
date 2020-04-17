e59c6bc086dabd32492464248ff8ba4b90f89ce8
ramzysaad <57734793+ramzysaad@users.noreply.github.com>
2020-04-17 00:27:41 -0400

    javasc
    javasc

--- AIRoO/app/js/Blockly/print.js
+++ AIRoO/app/js/Blockly/print.js
@@ -2,11 +2,19 @@ Blockly.Blocks['print'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Print")
-        .appendField(new Blockly.FieldTextInput("\"  \""), "NAME");
+        .appendField(new Blockly.FieldTextInput("\" \""), "NAME");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(15);
+    this.setColour(20);
  this.setTooltip("");
  this.setHelpUrl("");
   }
-};// JavaScript Document
+};
+
+
+Blockly.JavaScript['print'] = function(block) {
+  var text_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
--- AIRoO/app/js/Blockly/printWithTime.js
+++ AIRoO/app/js/Blockly/printWithTime.js
@@ -2,14 +2,25 @@ Blockly.Blocks['printwithtime'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Print")
-        .appendField(new Blockly.FieldTextInput("\"  \""), "NAME")
+        .appendField(new Blockly.FieldTextInput("\" \""), "NAME")
         .appendField(" For ")
-        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"], ["4","4"], ["5","5"], ["10","10"], ["20","20"]]), "T")
-        .appendField(new Blockly.FieldDropdown([["Minute/s","Minute/s"], ["Seconds","Seconds"]]), "M");
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"], ["4","4"], ["5","5"], ["20","20"], ["30","30"], ["40","40"], ["50","50"]]), "option")
+        .appendField(new Blockly.FieldDropdown([["Minutes(s)","Minutes"], ["Second(s)","Second"], ["option","OPTIONNAME"]]), "option2");
     this.setPreviousStatement(true, null);
-    this.setNextStatement(true, "Number");
-    this.setColour(15);
+    this.setNextStatement(true, null);
+    this.setColour(20);
  this.setTooltip("");
  this.setHelpUrl("");
   }
+};
+
+
+
+Blockly.JavaScript['printwithtime'] = function(block) {
+  var text_name = block.getFieldValue('NAME');
+  var dropdown_option = block.getFieldValue('option');
+  var dropdown_option2 = block.getFieldValue('option2');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
 };
