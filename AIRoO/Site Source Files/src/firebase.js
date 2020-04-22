696be7ebef799eeb3d7aefc01d0849d3f86e746f
Long Pham <lnp5131@psu.edu>
2020-04-21 21:18:18 -0400

    Update firebase.js

--- AIRoO/Site Source Files/src/firebase.js
+++ AIRoO/Site Source Files/src/firebase.js
@@ -1,12 +1,12 @@
 // firebase.js
 import firebase from 'firebase';
 const config = {
-  apiKey: "AIzaSyBSzA1fsFpm4vTmUnhgBI3I9poJWTMf5KE",
-  authDomain: "airoo-54944.firebaseapp.com",
-  databaseURL: "https://airoo-54944.firebaseio.com",
-  projectId: "airoo-54944",
-  storageBucket: "airoo-54944.appspot.com",
-  messagingSenderId: "453252867616"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
