7bbbfed164c3285184369975f019724f2e271bae
Long Pham <lnp5131@psu.edu>
2020-04-26 22:21:55 -0400

    Directory Update & Linked JS Files from Old

---
+++ Site Build Files/precache-manifest.202184aba5dcfcb01f1db56c79086aef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bc863de41e5e88b2e65",
+    "url": "/static/css/main.6c09064a.chunk.css"
+  },
+  {
+    "revision": "2bc863de41e5e88b2e65",
+    "url": "/static/js/main.2bc863de.chunk.js"
+  },
+  {
+    "revision": "ae7886b7f79d51fc0890",
+    "url": "/static/js/1.ae7886b7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c897f4f3d73908218e0e74d9e14adfc3",
+    "url": "/static/media/p1.c897f4f3.png"
+  },
+  {
+    "revision": "a5f2a2e8540022910a00d2ad7095fdae",
+    "url": "/static/media/a1.a5f2a2e8.jpg"
+  },
+  {
+    "revision": "8914df533fcf3e3486bcaaa9695245de",
+    "url": "/static/media/a2.8914df53.jpg"
+  },
+  {
+    "revision": "85008d98c88eaab229f31b6498856dbe",
+    "url": "/static/media/a3.85008d98.jpg"
+  },
+  {
+    "revision": "afc03f1a7277f2dba6d870d824df0a4f",
+    "url": "/static/media/banner_c.afc03f1a.jpg"
+  },
+  {
+    "revision": "4e3cc4631a10a66784f675fbc25aa126",
+    "url": "/index.html"
+  }
+];
---
+++ Site Source Files/src/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch, withRouter } from 'react-router-dom';
+import firebase, {auth, provider} from './firebase'
+
+import './index.css';
+
+import Home from './components/Home/HomePrototype';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Conference from './components/Navigation/Conference';
+import Committee from './components/Navigation/Committee';
+import Section from './components/Navigation/Section';
+
+class AppRouter extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {user: null}
+		this.logOutUser = this.logOutUser.bind(this);
+	}
+	componentDidMount(){
+		auth.onAuthStateChanged(user => {
+			if(user){
+				this.setState({
+					user
+				});
+			}
+		});
+
+		var loadScript = function (src) {
+			var tag = document.createElement('script');
+			tag.async = false;
+			tag.src = src;
+			var body = document.getElementsByTagName('body')[0];
+			body.appendChild(tag);
+		}
+
+		loadScript("./js/b_script.js");
+		loadScript("./js/bootstrap.js");
+		loadScript("./js/easing.js");
+		loadScript("./js/jquery-2.2.3.min.js");
+		loadScript("./js/jQuery_menu.js");
+		loadScript("./js/move-top.js");
+		loadScript("./js/respond.min.js");
+		loadScript("./js/script_co.js");
+		loadScript("./js/scroll.js");
+		loadScript("./js/SmoothScroll.min.js")
+		loadScript("./js/suspend.js");;
+	}
+
+	logOutUser = () => {
+		firebase.auth().signOut()
+			.then(window.location = "/");
+	};
+
+	render(){
+		return(
+			<Router>
+				<div className="app">
+					<img src={require("./images/banner_c.jpg")} />
+					<nav className="main-nav">
+						{!this.state.user && 
+							<div>
+								<Link to="/login">Login</Link>
+								<Link to="/register">Register</Link>
+							</div>
+						}
+
+						{this.state.user &&
+							<div>
+								<Link to="/conference">Conference</Link>
+								<Link to="/committee">Committee</Link>
+								<Link to="/section">Section</Link>
+								<a href="#!" onClick={this.logOutUser}>Logout</a>
+							</div>
+						}
+					</nav>
+
+					<Switch>
+						<Route path="/" exact render={() => <Home user={this.state.user}/>} />
+						<Route path="/login" exact component={Login} />
+						<Route path="/register" exact component={Register} />
+						<Route path="/conference" exact component={Conference} />
+						<Route path="/committee" exact component={Committee} />
+						<Route path="/section" exact component={Section} />
+						<Route component={NoMatch} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+}
+
+const NoMatch = ({location}) => <div>No route match for {location.pathname}</div>;
+
+ReactDOM.render(
+	<AppRouter />, 
+	document.getElementById('root')
+);
