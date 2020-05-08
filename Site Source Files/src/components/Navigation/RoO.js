877090b8d42c38614c02b58ed55614a319582cf9
ramzysaad <57734793+ramzysaad@users.noreply.github.com>
2020-05-08 10:47:29 -0400

    roo
    roo

--- Site Source Files/src/components/Navigation/RoO.js
+++ Site Source Files/src/components/Navigation/RoO.js
@@ -80,48 +80,211 @@ class RoO extends React.Component {
         return (
             <div className="RoO-container">
                 {this.props.user ?
-                    <div>
-                        <button onClick={this.generateCode}>Convert</button>
-                        <BlocklyComponent ref={e => this.simpleWorkspace = e}
-                                          readOnly={false} trashcan={true} media={'media/'}
-                                          move={{
-                                              scrollbars: true,
-                                              drag: true,
-                                              wheel: true
-                                          }}
-                                          initialXml={`<xml xmlns="http://www.w3.org/1999/xhtml">
-                                            <block type="controls_ifelse" x="0" y="0"></block>
-                                            </xml>`}>
-                            <Block type="test_react_field"/>
-                            <Block type="test_react_date_field"/>
-                            <Block type="controls_ifelse"/>
-                            <Block type="logic_compare"/>
-                            <Block type="logic_operation"/>
-                            <Block type="controls_repeat_ext">
-                                <Value name="TIMES">
-                                    <Shadow type="math_number">
-                                        <Field name="NUM">10</Field>
-                                    </Shadow>
-                                </Value>
-                            </Block>
-                            <Block type="logic_operation"/>
-                            <Block type="logic_negate"/>
-                            <Block type="logic_boolean"/>
-                            <Block type="logic_null" disabled="true"/>
-                            <Block type="logic_ternary"/>
-                            <Block type="text_charAt">
-                                <Value name="VALUE">
-                                    <Block type="variables_get">
-                                        <Field name="VAR">text</Field>
-                                    </Block>
-                                </Value>
-                            </Block>
-                        </BlocklyComponent>
-                    </div>
-                    : <Route
-                        render={props => <Redirect to={{pathname: '/'}}/>}
-                    />
-                }
+                
+<xml id="toolbox" style="display:none;">
+
+
+    <category name="Text" colour="#5CA68D">
+        <block type="text_charAt">
+            <mutation at="true"></mutation>
+            <field name="WHERE">FROM_START</field>
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR" id="G1L.Z/,8={hV#-q8hx|k" variabletype="">text</field>
+                </block>
+            </value>
+        </block>
+        <block type="text">
+            <field name="TEXT"></field>
+        </block>
+        <block type="text_append">
+            <field name="VAR" id="ZZWW3YzH`I^K4Q[]7KB-" variabletype="">item</field>
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT"></field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_length">
+            <value name="VALUE">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_isEmpty">
+            <value name="VALUE">
+                <shadow type="text">
+                    <field name="TEXT"></field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_indexOf">
+            <field name="END">FIRST</field>
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR" id="G1L.Z/,8={hV#-q8hx|k" variabletype="">text</field>
+                </block>
+            </value>
+            <value name="FIND">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_join">
+            <mutation items="2"></mutation>
+        </block>
+        <block type="text_getSubstring">
+            <mutation at1="true" at2="true"></mutation>
+            <field name="WHERE1">FROM_START</field>
+            <field name="WHERE2">FROM_START</field>
+            <value name="STRING">
+                <block type="variables_get">
+                    <field name="VAR" id="G1L.Z/,8={hV#-q8hx|k" variabletype="">text</field>
+                </block>
+            </value>
+        </block>
+        <block type="text_changeCase">
+            <field name="CASE">UPPERCASE</field>
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_trim">
+            <field name="MODE">BOTH</field>
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_print">
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="text_prompt_ext">
+            <mutation type="TEXT"></mutation>
+            <field name="TYPE">TEXT</field>
+            <value name="TEXT">
+                <shadow type="text">
+                    <field name="TEXT">abc</field>
+                </shadow>
+            </value>
+        </block>
+    </category>
+
+
+    <category name="Loops" colour="120">
+        <block type="controls_repeat_ext"></block>
+        <block type="controls_whileUntil"></block>
+        <block type="controls_for">
+
+
+        </block>
+        <block type="controls_forEach"></block>
+        <block type="controls_flow_statements"></block>
+    </category>
+
+
+    <category name="Lists" colour="260">
+        <block type="lists_create_with"></block>
+        <block type="lists_repeat"></block>
+        <block type="lists_length"></block>
+        <block type="lists_isEmpty"></block>
+        <block type="lists_indexOf">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_getIndex">
+            <value name="VALUE">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_setIndex">
+            <value name="LIST">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_getSublist">
+            <value name="LIST">
+                <block type="variables_get">
+                    <field name="VAR">{listVariable}</field>
+                </block>
+            </value>
+        </block>
+        <block type="lists_split">
+            <value name="DELIM">
+                <shadow type="text">
+                    <field name="TEXT">,</field>
+                </shadow>
+            </value>
+        </block>
+        <block type="lists_sort"></block>
+    </category>
+
+
+  < >
+  < />
+
+  <sep></sep>
+  <sep></sep>
+  <sep></sep>
+  <category name="Variables" colour="#A65C81" custom="VARIABLE"></category>
+  <category name="Functions" colour="#9A5CA6" custom="PROCEDURE"></category>
+  
+  <sep></sep>
+  <sep></sep>
+  <sep></sep>
+  
+  <category name="Actions" colour="#915848">
+   <block type="say"></block>
+    <block type="print"></block>
+    <block type="printwithtime">
+    <field name="M">20</field>
+    <field name="T">Seconds</field>
+    </block>
+   <block type="suspend"></block>
+   <block type="recording"></block>
+  </category>
+  
+  <category name="Do" colour="#8f9545">
+  <block type="do">
+    <field name="Roll Call">Roll Call</field>
+    </block>
+    
+    <block type="dolist">
+    <field name="Roll Call">Roll Call</field>
+    </block>
+    
+   </category>
+   
+   
+   
+  
+  
+ 
+  
+   <category name="On Click" colour="#948846">
+  <block type="onclick">
+  </category>
+  
+  
+  
+  
+</xml>
             </div>
         );
     }
